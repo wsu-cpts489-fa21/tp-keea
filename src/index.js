1d01ce4674d16d421a83c8d6c0536767ff3122ed
Chris Hundhausen <hundhaus@wsu.edu>
2021-10-08 12:09:10 -0700

    Updated create-react app starter code for SpeedScore

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '@fortawesome/fontawesome-svg-core/styles.css'
-import 'styles/index.css'
-import App from './App';
+import './styles/index.css'
+import App from './components/App.js';
 
 ReactDOM.render(<App />,
   document.getElementById('root')
