aa0dadfad4e3d5b34fb4e7cfb6fa336d5ebeeeec
Chris Hundhausen <hundhaus@wsu.edu>
2021-10-08 13:02:17 -0700

    Implement and test NavBar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,22 @@
 import NavBar from './NavBar.js';
-import SideMenu from './SideMenu.js';
+/* import SideMenu from './SideMenu.js';
 import ModeBar from './ModeBar.js';
 import FloatingButton from './FloatingButton.js';
-import LoginPage from './LoginPage.js';
+import LoginPage from './LoginPage.js'; */
 
 
 function App() {
   return (
     <div>
       <NavBar title="Welcome to SpeedScore"/>
+      {/* 
       <SideMenu />
       <ModeBar />
       <FloatingButton />
       <LoginPage />
+      */}
     </div>
-  );
+  ); 
 }
 
 export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSave, faBars, faSearch } from '@fortawesome/free-solid-svg-icons'
+import logo from '../images/sslogo.png'
+
+
+class NavBar extends React.Component {
+    
+    render() {
+       return (
+        <header className="navbar">  
+        <a id="sLink" className="skip-link" tabIndex="0">
+         Skip to content</a>
+         <button id="menuBtn" type="button" className="navbar-btn hidden" 
+            title="Menu" aria-controls="sideMenu" 
+            aria-label="Actions" aria-haspopup="true" 
+            aria-expanded="false">
+            <FontAwesomeIcon icon={faBars} className="navbar-btn-icon"/>
+          </button>
+          <img src={logo} className="navbar-app-icon" 
+            alt="SpeedScore logo" />
+           <h1 id="appName" className="navbar-title">{this.props.title}</h1> 
+           <div className="navbar-right-items">
+                <input id="searchBox" className="form-control hidden" 
+                aria-label="Search Rounds" size="30"
+                type="search" />
+                <button id="searchBtn" type="button" className="navbar-btn hidden" 
+                    aria-label="Open Rounds Search">
+                    <FontAwesomeIcon icon={faSearch} className="navbar-btn-icon"/>
+                </button>
+                <button id="profileBtn" type="button" 
+                  className="navbar-btn navbar-profile-btn hidden" 
+                  aria-label="Account and Profile Settings">
+                </button> 
+            </div>
+      </header>
+    ); 
+  }
+}
+
+export default NavBar;
