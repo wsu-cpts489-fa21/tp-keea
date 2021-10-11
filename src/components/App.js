9cbb038ce5edbfcf9d3d7788a74854cd625a40a3
Chris Hundhausen <hundhaus@wsu.edu>
2021-10-11 13:02:21 -0700

    Add menuOpen, modalOpen, and userId state variables and setters to App component Pass as props to subcomponents

--- src/components/App.js
+++ src/components/App.js
@@ -8,23 +8,49 @@ class App extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {mode: AppMode.LOGIN};
+    this.state = {mode: AppMode.LOGIN,
+                  menuOpen: false,
+                  modalOpen: false,
+                  userId: ""};
   }
 
   setMode = (newMode) => {
     this.setState({mode: newMode});
   }
 
+  toggleMenuOpen = () => {
+    this.setState(prevState => ({menuOpen: !prevState.menuOpen}));
+  }
+
+  toggleModalOpen = () => {
+    this.setState(prevState => ({dialogOpen: !prevState.dialogOpen}));
+  }
+
+  setUserId = (Id) => {
+    this.setState({userId: Id});
+  }
+
   render() {
     return (
       <>
         <NavBar mode={this.state.mode}
-                />
+                menuOpen={this.state.menuOpen}
+                toggleMenuOpen={this.state.toggleMenuOpen}
+                modalOpen={this.state.modalOpen}
+                toggleModalOpen={this.toggleModalOpen}
+                userId={this.state.userId}
+                setUserId={this.setUserid} />
         {this.state.mode !== AppMode.LOGIN ? 
           <ModeTabs mode={this.state.mode}
-                    setMode={this.setMode} /> 
+                    setMode={this.setMode} 
+                    menuOpen={this.state.menuOpen}
+                    modalOpen={this.state.modalOpen}/> 
             : null }
-        <LoginPage setMode={this.setMode} />
+        <LoginPage changeMode={this.handleChangeMode}
+                   menuOpen={this.state.menuOpen}
+                   modalOpen={this.state.dialogOpen}
+                   toggleModalOpen={this.state.toggleModalOpen} 
+                   userid={this.state.userId}/>
       </>
     ); 
   }
