aa46cd5601ceb7d07f02c61550a83d7890dbb949
Chris Hundhausen <hundhaus@wsu.edu>
2021-10-12 15:24:39 -0700

    Updated ModeTabs logic to align with book discussion

--- src/components/ModeTabs.js
+++ src/components/ModeTabs.js
@@ -3,36 +3,41 @@ import AppMode from './AppMode';
 
 class ModeTabs extends React.Component {
     render() {
-        const tabClasses = "modetab-btn" + (this.props.menuOpen ? " disabled" : "");
         return (this.props.mode !== AppMode.LOGIN && !this.props.modalOpen ? 
-        <div id="modeTabs" className="modetab-container" role="tablist" 
-            aria-label="App Modes">
+        <div id="modeTabs" 
+             className={"modetab-container" + this.props.menuOpen ? " disabled" : ""}  
+             role="tablist" 
+             aria-label="App Modes">
             <button id="feedMode" type="button" 
-                className={tabClasses +  
+                className={"modetab-btn" +  
                     (this.props.mode === AppMode.FEED ? "modetab-selected" : "") }
                 role="tab" tabIndex="0" aria-selected="true" 
-                aria-controls="feedModeTab">
+                aria-controls="feedModeTab"
+                onclick={() => this.props.setMode(AppMode.FEED)}>
             Feed
             </button>
             <button id="roundsMode" type="button" 
-                className={tabClasses +  
+                className={"modetab-btn" +  
                     (this.props.mode === AppMode.ROUNDS ? "modetab-selected" : "") } 
                 role="tab" tabIndex="-1" aria-selected="false" 
-                aria-controls="roundsModeTab">
+                aria-controls="roundsModeTab"
+                onclick={() => this.props.setMode(AppMode.ROUNDS)}>
             Rounds
             </button>
             <button id="coursesMode" type="button" 
-                className={tabClasses +  
-                    (this.props.mode === AppMode.COURSES ? "modetab-selected" : "") } 
+                className={"modetab-btn" +  
+                    (() => this.props.mode === AppMode.COURSES ? "modetab-selected" : "") } 
                 role="tab" tabIndex="-1" aria-selected="false" 
-                aria-controls="coursesModeTab">
+                aria-controls="coursesModeTab"
+                onclick={() => this.props.setMode(AppMode.COURSES)}>
             Courses
             </button>
             <button id="buddiesMode" type="button" 
-                className={tabClasses +  
+                className={"modetab-btn" +  
                     (this.props.mode === AppMode.BUDDIES ? "modetab-selected" : "") } 
                 role="tab" tabIndex="-1" aria-selected="false" 
-                aria-controls="buddiesModeTab">
+                aria-controls="buddiesModeTab"
+                onclick={() => this.props.setMode(AppMode.BUDDIES)}>
             Buddies
             </button>
         </div>  : null 
