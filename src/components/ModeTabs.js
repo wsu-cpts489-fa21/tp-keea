d35e42a6d3486096578e8859efe3972704620a1f
Chris Hundhausen <hundhaus@wsu.edu>
2021-10-12 14:28:25 -0700

    Implement ModeTabs  render() method with conditional logic

--- src/components/ModeTabs.js
+++ src/components/ModeTabs.js
@@ -1,36 +1,42 @@
 import React from 'react';
+import AppMode from './AppMode';
 
 class ModeTabs extends React.Component {
     render() {
-      return(
+        const tabClasses = "modetab-btn" + (this.props.menuOpen ? " disabled" : "");
+        return (this.props.mode !== AppMode.LOGIN && !this.props.modalOpen ? 
         <div id="modeTabs" className="modetab-container" role="tablist" 
             aria-label="App Modes">
             <button id="feedMode" type="button" 
-                className="modetab-btn modetab-selected" 
+                className={tabClasses +  
+                    (this.props.mode === AppMode.FEED ? "modetab-selected" : "") }
                 role="tab" tabIndex="0" aria-selected="true" 
                 aria-controls="feedModeTab">
             Feed
             </button>
             <button id="roundsMode" type="button" 
-                className="modetab-btn" 
+                className={tabClasses +  
+                    (this.props.mode === AppMode.ROUNDS ? "modetab-selected" : "") } 
                 role="tab" tabIndex="-1" aria-selected="false" 
                 aria-controls="roundsModeTab">
             Rounds
             </button>
             <button id="coursesMode" type="button" 
-                className="modetab-btn" 
+                className={tabClasses +  
+                    (this.props.mode === AppMode.COURSES ? "modetab-selected" : "") } 
                 role="tab" tabIndex="-1" aria-selected="false" 
                 aria-controls="coursesModeTab">
             Courses
             </button>
             <button id="buddiesMode" type="button" 
-                className="modetab-btn" 
+                className={tabClasses +  
+                    (this.props.mode === AppMode.BUDDIES ? "modetab-selected" : "") } 
                 role="tab" tabIndex="-1" aria-selected="false" 
                 aria-controls="buddiesModeTab">
             Buddies
             </button>
-      </div>
-      );
+        </div>  : null 
+        );
     }
 }
 
