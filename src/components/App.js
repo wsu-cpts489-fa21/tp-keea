85b7eec35794763d6de90d160138d0a371a60fff
Chris Hundhausen <hundhaus@wsu.edu>
2021-10-13 14:00:47 -0700

    Update LoginPage component Users can log with valid email and password Fedback is presented if email and/or password invalid

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,7 @@ class App extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {mode: AppMode.FEED,
+    this.state = {mode: AppMode.LOGIN,
                   menuOpen: false,
                   modalOpen: false,
                   userId: ""};
