c83a565d02f0a7b9bd2dbb86ae6fa0f5224d78fc
Chris Hundhausen <hundhaus@wsu.edu>
2021-10-18 07:42:12 -0700

    Update RoundsTable component Add renderTable() helper function

--- src/components/RoundsTable.js
+++ src/components/RoundsTable.js
@@ -1,16 +1,49 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSort  } from '@fortawesome/free-solid-svg-icons'
+import { faSort, faTrash, faEye, faEdit } from '@fortawesome/free-solid-svg-icons'
 
 class RoundsTable extends React.Component {
 
+  renderTable = () => {
+    const table = [];
+    for (let r = 0; r < this.props.rounds.length; ++r) {
+      const thisRoundNum = this.props.rounds[r].roundNum;
+      table.push(
+        <tr key={thisRoundNum.toString()}>
+          <td>{this.props.rounds[r].date}</td>
+          <td>{this.props.rounds[r].course}</td>
+          <td>{(Number(this.props.rounds[r].strokes) + 
+                Number(this.props.rounds[r].minutes)) +
+                ":" + this.props.rounds[r].seconds + " (" + 
+                this.props.rounds[r].strokes + 
+                " in " + this.props.rounds[r].minutes + ":" + 
+                this.props.rounds[r].seconds + ")"}
+          </td>
+          <td><button onClick={this.props.menuOpen ? null : () => 
+                  this.props.initiateEditRound(thisRoundNum)}>
+                <FontAwesomeIcon icon={faEye} /> 
+                <FontAwesomeIcon icon={faEdit} /> 
+              </button></td>
+          <td><button onClick={this.props.menuOpen ? null : 
+            () => this.props.initiateDeleteRound(thisRoundNum)}>
+                <FontAwesomeIcon icon={faTrash}/>
+              </button></td>
+        </tr> 
+      );
+    }
+    return table;
+  }
+
     render() {
-        return(
+      return(
         <div id="roundsModeTab" className="mode-page" role="tabpanel"
-           aria-label="Rounds Tab" tabIndex="0">
+            aria-label="Rounds Tab" tabIndex="0">
         <h1 className="mode-page-header">Rounds</h1>
         <table id="roundsTable" className="table table-hover caption-top">
-          <caption id="roundsTableCaption" aria-live="polite">Rounds Table</caption>
+          <caption id="roundsTableCaption" aria-live="polite">
+            {"Table displaying " + this.props.rounds.length  + " speedgolf round" + 
+              (this.props.rounds.length !== 1 ? "s" : "")}
+          </caption>
           <thead className="table-light">
             <tr>
             <th scope="col" role="columnheader" 
@@ -46,13 +79,15 @@ class RoundsTable extends React.Component {
             </tr>
           </thead>
           <tbody>
-            <tr>
-            <td colSpan="5" scope="rowgroup"><i>No rounds added yet</i></td>
-            </tr> 
+            {this.props.rounds === null || this.props.rounds.length === 0 ? 
+              <tr>
+                <td colSpan="5" scope="rowgroup"><i>No rounds logged</i></td>
+              </tr> : this.renderTable()
+            }
           </tbody>
         </table>        
       </div>
-        );
+      );
     }   
 }
 
