b5e72e485cd738b0205c1d931264118636ebd30e
Chris Hundhausen <hundhaus@wsu.edu>
2021-10-26 08:32:53 -0700

    Updated server.js so that path is correct for both local and remote deployment

--- server.js
+++ server.js
@@ -3,17 +3,17 @@
 //The following code imports necessary dependencies and initializes
 //variables used in the server middleware.
 //////////////////////////////////////////////////////////////////////////
-import session from 'express-session';
-import passport from 'passport';
 //import path from 'path';
 import { URL } from 'url';
 import express from 'express';
 import passportConfig from './passport/config.js';
 import authRoute from './routes/authRoutes.js';
-import { Server } from 'http';
 const PORT = process.env.PORT || process.env.LOCAL_PORT;
 const app = express(); //Instantiate express app
-const buildPath = new URL('client/build/', import.meta.url).pathname;
+const buildPath = (PORT === process.env.PORT) ?
+  new URL('client/build/', import.meta.url).pathname :
+  (new URL('client/build/', import.meta.url).pathname).substring(1);
+  
 
 //////////////////////////////////////////////////////////////////////////
 //INITIALIZE EXPRESS APP
@@ -25,5 +25,5 @@ passportConfig(app); //Configure session and passport
 app
   .use(express.static(buildPath))
   .use(authRoute)
-  .listen(PORT, () => console.log(`Listening on ${PORT}`))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
   
