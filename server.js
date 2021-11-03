2ff483acf3e0f311059706d30f650d1cde73f1eb
Chris Hundhausen <hundhaus@wsu.edu>
2021-11-03 11:43:01 -0700

    Update app to connect with remote MongoDB instance

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ const buildPath = (PORT === process.env.PORT) ?
   new URL('client/build/', import.meta.url).pathname :
   (new URL('client/build/', import.meta.url).pathname).substring(1);
 import mongoose from 'mongoose';
-const connectStr = 'mongodb://localhost:27017/appdb';
-
-
+//const connectStr = 'mongodb://localhost:27017/appdb'; //Local
+const connectStr = 'mongodb+srv://' + process.env.MONGODB_USER + ':' + 
+                  process.env.MONGODB_PW + process.env.MONGODB_CSTRING; //Remote DB
 //////////////////////////////////////////////////////////////////////////
 //MONGOOSE SET-UP
 //The following code sets up the app to connect to a MongoDB database
