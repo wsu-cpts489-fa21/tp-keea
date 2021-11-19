380e6725f3ad285eae7d77d3b62d9014f5f591b3
eaguiler <elisha.aguilera@wsu.edu>
2021-11-18 21:34:45 -0800

    Stubbed delete playwright test

---
+++ test/Playwright/deleteTest.test.js
@@ -0,0 +1,13 @@
+import {test, expect} from "@playwright/test";
+
+test('Delete mouse input test', async ({page}) => {
+    // Stub for delete process with mouse inputs
+    page.goto('http://localhost:8081/');
+    // Further steps
+});
+
+test('Delete keyboard input test', async ({page}) => {
+    // Stub for delete process with keyboard inputs
+    page.goto('http://localhost:8081/');
+    // Further steps
+});
