0847df61bf57e9d02dbff16416ad80b632dd0be2
drwontoned <ksuzara@yahoo.com>
2021-11-20 00:28:16 -0800

    debugging for heroku

--- server.js
+++ server.js
@@ -39,7 +39,8 @@ mongoose.connect(connectStr, {useNewUrlParser: true, useUnifiedTopology: true})
 
 passportConfig(app); //Configure session and passport
 app
-  .use(express.static(path.join(__dirname, 'client', 'build')))
+  //.use(express.static(path.join(__dirname, 'client', 'build')))
+  .use(buildPath)
   .use(express.json({limit: '20mb'}))
   .use(authRoute)
   .use(userRoute)
