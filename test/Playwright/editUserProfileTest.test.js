845abbe0faa90938a638af2ba4494eba7e304db4
Malachi Potts <malachi.potts@wsu.edu>
2021-11-20 20:36:38 -0800

    Added mouse interaction test for edit profile

--- test/Playwright/editUserProfileTest.test.js
+++ test/Playwright/editUserProfileTest.test.js
@@ -1,5 +1,18 @@
 import {test, expect} from "@playwright/test";
 
 test('Edit profile mouse input test', async ({page}) => {
-    
+    await page.goto('http://localhost:8081/');
+    await page.fill('#email', 'malachipotts@gmail.com');
+    await page.fill('#password', 'a1AAAAAA');
+    await page.click('#loginBtn');
+    await page.click('#profileBtn');
+    await page.fill('#profileSecurityQuestion', 'New security question TEST');
+    await page.fill('#profileSecurityAnswer', 'New security answer TEST');
+    await page.click('#sgSettingsBtn');
+    await page.fill('#sgBio', 'I am a Speedgolf tester!');
+    await page.fill('#sgHomeCourser', 'That one mini golf place');
+    await page.fill('#sgClubComments', 'I got all the clubs');
+    const clubs = page.locator('#sgClubComments');
+    await expect(clubs).toHaveText('I got all the clubs');
+    await page.click('#submitUpdateProfileBtn');
 });
