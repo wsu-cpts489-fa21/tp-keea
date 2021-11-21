2a5ffd2a8908c3c3970609d483c572d2367ef743
Malachi Potts <malachi.potts@wsu.edu>
2021-11-20 20:51:42 -0800

    Added keyboard interaction test for edit profile

--- test/Playwright/editUserProfileTest.test.js
+++ test/Playwright/editUserProfileTest.test.js
@@ -2,7 +2,7 @@ import {test, expect} from "@playwright/test";
 
 test('Edit profile mouse input test', async ({page}) => {
     await page.goto('http://localhost:8081/');
-    await page.fill('#email', 'malachipotts@gmail.com');
+    await page.fill('#email', 'keeatesting@gmail.com');
     await page.fill('#password', 'a1AAAAAA');
     await page.click('#loginBtn');
     await page.click('#profileBtn');
@@ -15,4 +15,44 @@ test('Edit profile mouse input test', async ({page}) => {
     const clubs = page.locator('#sgClubComments');
     await expect(clubs).toHaveText('I got all the clubs');
     await page.click('#submitUpdateProfileBtn');
+});
+
+test('Edit profile keyboard input test', async ({page}) => {
+    await page.goto('http://localhost:8081/');
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Tab');
+    await page.keyboard.type('keeatesting@gmail.com', {delay: 100});
+    await page.keyboard.press('Tab');
+    await page.keyboard.type('a1AAAAAA', {delay: 100});
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Enter');
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Enter');
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Tab');
+    await page.keyboard.type('New security question TEST', {delay: 100});
+    await page.keyboard.press('Tab');
+    await page.keyboard.type('New security answer TEST', {delay: 100});
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Enter');
+    await page.keyboard.press('Tab');
+    await page.keyboard.type('I am a Speedgolf tester!', {delay: 100});
+    await page.keyboard.type('That one mini golf place', {delay: 100});
+    await page.keyboard.type('I got all the clubs', {delay: 100});
+    const clubs = page.locator('#sgClubComments');
+    await expect(clubs).toHaveText('I got all the clubs');
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Enter');
 });
