3cc9cd074d2999f5ccc33bfee442fc67386decab
eaguiler <elisha.aguilera@wsu.edu>
2021-11-30 23:38:16 -0800

    Added course floating button, form, fields

---
+++ client/src/components/CoursesMode.js
@@ -0,0 +1,10 @@
+// Enumerator for all Courses modes.
+const CoursesMode = {
+    COURSESTABLE: "CoursesTable",
+    ADDCOURSE: "AddCourse",
+    EDITCOURSE: "EditCourse",
+};
+
+Object.freeze(CoursesMode);
+
+export default CoursesMode;
--- client/src/components/CoursesPage.js
+++ client/src/components/CoursesPage.js
@@ -1,25 +1,53 @@
 import React from 'react';
 import logo from '../images/sslogo2.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import CoursesMode from './CoursesMode.js';
+import CourseForm from './CourseForm';
 import FloatingButton from './FloatingButton.js';
 
 class CoursesPage extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            mode: CoursesMode.COURSESTABLE,
+            deletedId: -1,
+            editId: -1
+        }
+    }
+
+    setMode = (newMode) => {
+        this.setState({
+            mode: newMode,
+        });
+    }
+
+    toggleModalOpen = () => {
+        // To be filled...
     }
 
     render() {
-        return (
-            <>
-                <div id="coursesModeTab" className="mode-page" role="tabpanel" aria-label="Courses Tab" tabIndex="0">
-                    <h1 className="mode-page-header">Courses</h1>
-                    <p className="mode-page-content">This page is under construction.</p>
-                    <img className="mode-page-icon" src={logo} alt="SpeedScore logo"></img>
-                </div>
-                <FloatingButton icon="calendar" label={"Add Course"} menuOpen={this.props.menuOpen} action={() => this.setState({})} />
-            </>
-        );
+        switch (this.state.mode) {
+            case CoursesMode.COURSESTABLE:
+                return (
+                    <>
+                        <div id="coursesModeTab" className="mode-page" role="tabpanel" aria-label="Courses Tab" tabIndex="0">
+                            <h1 className="mode-page-header">Courses</h1>
+                            <p className="mode-page-content">This page is under construction.</p>
+                            <img className="mode-page-icon" src={logo} alt="SpeedScore logo"></img>
+                        </div>
+                        <FloatingButton icon="calendar" label={"Add Course"} menuOpen={this.props.menuOpen} action={() => this.setState({mode: CoursesMode.ADDCOURSE}, this.props.toggleModalOpen)} />
+                    </>
+                );
+            case CoursesMode.ADDCOURSE:
+                return (
+                    <CourseForm mode={this.state.mode} courseData={null} setMode={this.setMode} toggleModalOpen={this.props.toggleModalOpen} />
+                );
+            case CoursesMode.EDITCOURSE:
+                return (
+                    <>
+                    </>
+                );
+        }
     }
 }
 
