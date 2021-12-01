b608c5c50578c4f66b2d6dab5923bfa5f6f8ba8d
eaguiler <elisha.aguilera@wsu.edu>
2021-11-30 17:33:16 -0800

    Initial commit, floating button, setup

--- client/src/components/CoursesPage.js
+++ client/src/components/CoursesPage.js
@@ -1,19 +1,26 @@
 import React from 'react';
-import logo from '../images/sslogo2.png'
+import logo from '../images/sslogo2.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import FloatingButton from './FloatingButton.js';
 
 class CoursesPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
 
     render() {
         return (
-            <div id="coursesModeTab" className="mode-page" role="tabpanel"
-                 aria-label="Courses Tab" tabIndex="0">
-                <h1 className="mode-page-header">Courses</h1>
-                <p className="mode-page-content">This page is under construction.</p>
-                <img className="mode-page-icon" 
-                     src={logo} alt="SpeedScore logo"></img>
-            </div>
+            <>
+                <div id="coursesModeTab" className="mode-page" role="tabpanel" aria-label="Courses Tab" tabIndex="0">
+                    <h1 className="mode-page-header">Courses</h1>
+                    <p className="mode-page-content">This page is under construction.</p>
+                    <img className="mode-page-icon" src={logo} alt="SpeedScore logo"></img>
+                </div>
+                <FloatingButton icon="calendar" label={"Add Course"} menuOpen={this.props.menuOpen} action={() => this.setState({})} />
+            </>
         );
-    }   
+    }
 }
 
 export default CoursesPage;
