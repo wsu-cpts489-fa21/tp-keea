8ceff3f8f128c288607c6cbb7cf8983b1eced4a9
drwontoned <ksuzara@yahoo.com>
2021-12-08 15:14:21 -0800

    Review stuff built just not tested if implentation is working

--- client/src/components/CoursesTable.js
+++ client/src/components/CoursesTable.js
@@ -18,6 +18,10 @@ class CoursesTable extends React.Component {
         <tr key={r}>
           <td>{this.props.courses[r].courseName}</td>
           <td>{this.props.courses[r].averageOverall}</td>
+          <td><button onClick={this.props.menuOpen ? null : 
+            () => this.props.initiateReviewCourse(r)}>
+                <FontAwesomeIcon icon="pencil"/>
+              </button></td>
           <td>{Number(this.props.courses[r].roundCount)}</td>
           <td><button onClick={this.props.menuOpen ? null : () => 
               this.openCourseOnGoogle(r)}>
---
+++ client/src/components/ReviewTable.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+class CoursesTable extends React.Component {
+
+  renderTable = () => {
+    const table = [];
+    for (let r = 0; r < this.props.reviews.length; ++r) {
+      table.push(
+        <tr key={r}>
+          <td>{this.props.reviews[r].reviewerName}</td>
+          <td>{this.props.reviews[r].dateOfReview}</td>
+          <td>{this.props.reviews[r].reviewFriendliness}</td>
+        </tr> 
+      );
+    }
+    return table;
+  }
+
+    render() {
+      return(
+        <div id="reviewsModeTab" className="mode-page" role="tabpanel"
+            aria-label="Courses Tab" tabIndex="0">
+        <h1 className="mode-page-header">Reviewss</h1>
+        <table id="reviewsTable" className="table table-hover caption-top">
+          <caption id="reviewsTableCaption" aria-live="polite">
+            {"Table displaying " + this.props.reviews.length  + " course reviews" + 
+              (this.props.reviews.length !== 1 ? "s" : "")}
+          </caption>
+          <thead className="table-light">
+            <tr>
+            <th scope="col" role="columnheader" 
+                className="sortable-header cell-align-middle" 
+                aria-sort="none">
+                <button className="btn bg-transparent table-sort-btn" 
+                        aria-label="Sort ascending by name">
+                  <FontAwesomeIcon icon="sort" /> 
+                </button>Name
+            </th>
+            <th scope="col" role="columnheader" 
+                className="sortable-header cell-align-middle" 
+                aria-sort="none">
+                <button className="btn bg-transparent table-sort-btn" 
+                        aria-label="Sort ascending by date">
+                  <FontAwesomeIcon icon="sort" /> 
+                </button>Date
+            </th>
+            <th scope="col" className="cell-align-middle">
+              Friendliness
+            </th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.reviews === null || this.props.reviews.length === 0 ? 
+              <tr>
+                <td colSpan="5" scope="rowgroup"><i>No reviews logged</i></td>
+              </tr> : this.renderTable()
+            }
+          </tbody>
+        </table>        
+      </div>
+      );
+    }   
+}
+
+export default CoursesTable;
